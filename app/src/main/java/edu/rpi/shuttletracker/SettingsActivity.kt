23b097135d133f78987b7e3cc34c5076bc5b2b88
Miles <mgednie@gmail.com>
2021-11-05 16:28:09 -0400

    link page to menu

--- app/src/main/java/edu/rpi/shuttletracker/SettingsActivity.kt
+++ app/src/main/java/edu/rpi/shuttletracker/SettingsActivity.kt
@@ -1,7 +1,17 @@
 package edu.rpi.shuttletracker;
 
-public class SettingsActivity {
+import android.os.Bundle
+import android.view.View
+import com.google.android.gms.maps.SupportMapFragment
+import kotlinx.android.synthetic.main.activity_maps.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
+public class SettingsActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings)
+    }
     companion object {
         private const val MY_PERMISSIONS_REQUEST_LOCATION = 99
     }
