51aa121366f69eb5a731a169305a5569838d7dc7
Andy Pham <70230763+phama2@users.noreply.github.com>
2022-04-01 17:08:34 -0400

    Changed to staging

--- app/src/main/java/edu/rpi/shuttletracker/MapsActivity.kt
+++ app/src/main/java/edu/rpi/shuttletracker/MapsActivity.kt
@@ -129,7 +129,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .setStyle(NotificationCompat.BigTextStyle()
                 .bigText("Much longer text that cannot fit one line..."))
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setOngoing(true)
+            //.setOngoing(true)
 
 
         with(NotificationManagerCompat.from(this)) {
