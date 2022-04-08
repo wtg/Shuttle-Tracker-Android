29de51a6e055d7be3c0930cb7c89b7646e1930a9
erik <erikswanke01@gmail.com>
2022-04-08 17:03:53 -0400

    gave up on getting tests to work, going to test using the app itself instead

--- app/src/test/java/edu/rpi/shuttletracker/ExampleUnitTest.kt
+++ app/src/test/java/edu/rpi/shuttletracker/ExampleUnitTest.kt
@@ -3,15 +3,26 @@ package edu.rpi.shuttletracker
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.jupiter.api.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+//class ExampleUnitTest {
+//    @Test
+//    fun addition_isCorrect() {
+//        assertEquals(4, 2 + 2)
+//    }
+//}
+class ScheduleAPITest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun scheduleAPI_IsCorrect() {
+
+        val testsched = scheduleAPI("https://shuttletracker.app/schedule.json")[0]
+
     }
+
+
 }
