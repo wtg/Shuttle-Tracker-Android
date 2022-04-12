af427db25401c89cfb0c17df2955696543a05aeb
erik <erikswanke01@gmail.com>
2022-04-11 22:42:17 -0400

    located getJSONArray as the cause of crashing

--- app/src/main/java/edu/rpi/shuttletracker/SettingsActivity.kt
+++ app/src/main/java/edu/rpi/shuttletracker/SettingsActivity.kt
@@ -76,6 +76,7 @@ public class SettingsActivity: AppCompatActivity() {
         saveToggle(this, toggle.isChecked)
     }
     override fun onStart() {
+
         val toggle: SwitchMaterial = findViewById(R.id.colorblindSwitch)
         super.onStart()
         toggle.setChecked(loadToggle(this))
@@ -101,74 +102,5 @@ public class SettingsActivity: AppCompatActivity() {
         return sharedPreferences.getBoolean("toggle_value", true)
     }
 
-    private fun scheduleAPI(url: String) : ArrayList<Schedule>{
-        val semesterArray = ArrayList<Schedule>()
-        val thread = Thread(kotlinx.coroutines.Runnable {
-            kotlin.run {
-                val url = URL(url)
-                val jsonString = url.readText()
-                var jsonArray = JSONArray(jsonString)
-                for (i in 0 until jsonArray.length()) {
-                    val semSchedule = jsonArray.getJSONObject(i)
-                    val name = semSchedule.getString("name")
-                    val start = semSchedule.getString("start")
-                    val end = semSchedule.getString("end")
-                    val content = semSchedule.getJSONArray("content")
-                    val dayArray = ArrayList<Day>()
 
-                    //monday
-                    val monday = content.getJSONObject(0)
-                    val dayStart = monday.getString("start")
-                    val dayEnd = monday.getString("end")
-                    val dayObject = Day("Monday", dayStart, dayEnd)
-                    dayArray.add(dayObject)
-                    //tuesday
-                    val tuesday = content.getJSONObject(1)
-                    val tuesdayStart = tuesday.getString("start")
-                    val tuesdayEnd = tuesday.getString("end")
-                    val tuesdayObject = Day("Tuesday", tuesdayStart, tuesdayEnd)
-                    dayArray.add(tuesdayObject)
-                    //wednesday
-                    val wednesday = content.getJSONObject(2)
-                    val wednesdayStart = wednesday.getString("start")
-                    val wednesdayEnd = wednesday.getString("end")
-                    val wednesdayObject = Day("Wednesday", wednesdayStart, wednesdayEnd)
-                    dayArray.add(wednesdayObject)
-                    //thursday
-                    val thursday = content.getJSONObject(3)
-                    val thursdayStart = thursday.getString("start")
-                    val thursdayEnd = thursday.getString("end")
-                    val thursdayObject = Day("Thursday", thursdayStart, thursdayEnd)
-                    dayArray.add(thursdayObject)
-                    //friday
-                    val friday = content.getJSONObject(4)
-                    val fridayStart = friday.getString("start")
-                    val fridayEnd = friday.getString("end")
-                    val fridayObject = Day("Friday", fridayStart, fridayEnd)
-                    dayArray.add(fridayObject)
-                    //saturday
-                    val saturday = content.getJSONObject(5)
-                    val saturdayStart = saturday.getString("start")
-                    val saturdayEnd = saturday.getString("end")
-                    val saturdayObject = Day("Saturday", saturdayStart, saturdayEnd)
-                    dayArray.add(saturdayObject)
-                    //sunday
-                    val sunday = content.getJSONObject(6)
-                    val sundayStart = sunday.getString("start")
-                    val sundayEnd = sunday.getString("end")
-                    val sundayObject = Day("Sunday", sundayStart, sundayEnd)
-                    dayArray.add(sundayObject)
-                    //make schedule object for the day
-                    val scheduleObject = Schedule(name, start, end, dayArray)
-                    semesterArray.add(scheduleObject)
-                }
-                val currentsched = semesterArray.last()
-                for(i in 0 until currentsched.content.size){
-
-                }
-            }
-        })
-        thread.start()
-        return semesterArray
-    }
 }
