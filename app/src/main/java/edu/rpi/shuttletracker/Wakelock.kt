c208a9276dba140919a3091b0863ff814cd65e2e
edited <1084929656lkh@gmail.com>
2022-09-17 20:58:57 -0400

    Update Wakelock.kt

--- app/src/main/java/edu/rpi/shuttletracker/Wakelock.kt
+++ app/src/main/java/edu/rpi/shuttletracker/Wakelock.kt
@@ -7,9 +7,9 @@ import android.content.Intent
 import android.content.res.Resources
 import android.os.Build
 import android.os.IBinder
-import androidx.annotation.RequiresApi
 import java.util.*
 import kotlin.concurrent.scheduleAtFixedRate
+
 
 
 class Wakelock : Service(){
@@ -20,17 +20,19 @@ class Wakelock : Service(){
         return null
     }
 
-    @RequiresApi(Build.VERSION_CODES.S)
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val res : Resources = getResources()
-
-        var notification: Notification = Notification.Builder(this, "1")
+        var notificationbuilder: Notification.Builder = Notification.Builder(this, "1")
             .setSmallIcon(R.drawable.ic_menu_mylocation)
             .setContentTitle("Shuttle Tracker")//TODO get the string from xml file instead
             .setContentText("Who's the shuttle tracker now 🤔")
             .setOngoing(true)
-            .setForegroundServiceBehavior(Notification.FOREGROUND_SERVICE_IMMEDIATE)
-            .build()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            //setForegroundServiceBehavior is added in SDK.S, this is used to ensure device before android 12 compatibility
+            notificationbuilder.setForegroundServiceBehavior(Notification.FOREGROUND_SERVICE_IMMEDIATE)
+        }
+        var notification:Notification = notificationbuilder.build()
 
 
         println("Service Started")
