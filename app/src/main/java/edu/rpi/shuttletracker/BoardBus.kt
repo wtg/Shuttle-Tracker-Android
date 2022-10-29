268fb10426c9912a4bb5a02fbaf9c5f208f5338b
Josh Zheng <joshzheng77@gmail.com>
2022-10-28 23:54:12 -0400

    Began work on new homescreen widget
    Created a new class for a homescreen widget, and Android Studio generated new files. The XML files had a problem, but was able to fix it

---
+++ app/src/main/java/edu/rpi/shuttletracker/BoardBus.kt
@@ -0,0 +1,47 @@
+package edu.rpi.shuttletracker
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+// Intended functionality: A button that allows the user to board the bus. When the user clicks
+// on the button, it opens up the app and lets the user choose their desired bus ID
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class BoardBus : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    val widgetText = context.getString(R.string.appwidget_text)
+    // Construct the RemoteViews object
+    val views = RemoteViews(context.packageName, R.layout.board_bus)
+    views.setTextViewText(R.id.appwidget_text, widgetText)
+
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
