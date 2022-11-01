b2ba9b1c05a907b9a660741d18568ebe581567cb
Josh Zheng <joshzheng77@gmail.com>
2022-11-01 16:44:16 -0400

    Switched to RelativeView for widget
    I learned how to center a element in a view using the RelativeView. I switched from ConstraintView back to the RelativeView because ConstraintViews are not supported by RemoteViews and I need RemoteViews to implement the functionality of the widget

--- app/src/main/java/edu/rpi/shuttletracker/BoardBus.kt
+++ app/src/main/java/edu/rpi/shuttletracker/BoardBus.kt
@@ -12,11 +12,7 @@ import android.widget.RemoteViews
  * Implementation of App Widget functionality.
  */
 class BoardBus : AppWidgetProvider() {
-    override fun onUpdate(
-        context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetIds: IntArray
-    ) {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
@@ -32,11 +28,7 @@ class BoardBus : AppWidgetProvider() {
     }
 }
 
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
+internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
     val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.board_bus)
