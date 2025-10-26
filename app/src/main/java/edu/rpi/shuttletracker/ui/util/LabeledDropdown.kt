62d2820b6627e444e9839c2195c89077c97b3244
Alexander Wu <51211848+Pahina0@users.noreply.github.com>
2025-10-26 12:31:06 -0400

    change how schedule looks (#111)

--- app/src/main/java/edu/rpi/shuttletracker/ui/util/LabeledDropdown.kt
+++ app/src/main/java/edu/rpi/shuttletracker/ui/util/LabeledDropdown.kt
@@ -1,24 +1,19 @@
 package edu.rpi.shuttletracker.ui.util
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -43,53 +38,23 @@ fun <T> LabeledDropdown(
             onValueChange = {},
             readOnly = true,
             enabled = enabled,
-            label = { Text(label, color = MaterialTheme.colorScheme.onSurface) },
+            label = { Text(label) },
             trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded) },
             modifier = Modifier.fillMaxWidth().menuAnchor(MenuAnchorType.PrimaryNotEditable),
             singleLine = true,
-            colors =
-                TextFieldDefaults.colors(
-                    focusedContainerColor = MaterialTheme.colorScheme.surface,
-                    unfocusedContainerColor = MaterialTheme.colorScheme.surface,
-                    focusedIndicatorColor = MaterialTheme.colorScheme.outline,
-                    unfocusedIndicatorColor = MaterialTheme.colorScheme.outline,
-                    focusedTextColor = MaterialTheme.colorScheme.onSurface,
-                    unfocusedTextColor = MaterialTheme.colorScheme.onSurface,
-                ),
         )
 
         ExposedDropdownMenu(
             expanded = expanded,
             onDismissRequest = { expanded = false },
-            modifier = Modifier.border(1.dp, MaterialTheme.colorScheme.onPrimaryContainer),
         ) {
             items.forEach { item ->
-                val isSelected = item == selectedItem
                 DropdownMenuItem(
-                    text = {
-                        Text(
-                            itemText(item),
-                            color =
-                                if (isSelected) {
-                                    MaterialTheme.colorScheme.onPrimary
-                                } else {
-                                    MaterialTheme.colorScheme.onSurface
-                                },
-                        )
-                    },
+                    text = { Text(itemText(item)) },
                     onClick = {
                         onItemSelected(item)
                         expanded = false
                     },
-                    modifier =
-                        Modifier.fillMaxWidth()
-                            .background(
-                                if (isSelected) {
-                                    MaterialTheme.colorScheme.primary
-                                } else {
-                                    MaterialTheme.colorScheme.surface
-                                },
-                            ),
                 )
             }
         }
