09fe001dc9b5ae8565b65947d3a769e9bbdfad6d
Bryan Tran <bryan.tran0205@gmail.com>
2026-02-08 16:23:52 -0500

    move open app settings to settings page

--- app/src/main/java/edu/rpi/shuttletracker/ui/settings/SettingsScreen.kt
+++ app/src/main/java/edu/rpi/shuttletracker/ui/settings/SettingsScreen.kt
@@ -1,5 +1,8 @@
 package edu.rpi.shuttletracker.ui.settings
 
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,8 +13,8 @@ import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material.icons.outlined.Code
 import androidx.compose.material.icons.outlined.Contrast
 import androidx.compose.material.icons.outlined.Info
-import androidx.compose.material.icons.outlined.PrivacyTip
 import androidx.compose.material.icons.outlined.RestartAlt
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -29,6 +32,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -36,7 +40,6 @@ import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
 import com.ramcosta.composedestinations.generated.destinations.AboutScreenDestination
 import com.ramcosta.composedestinations.generated.destinations.DevMenuScreenDestination
-import com.ramcosta.composedestinations.generated.destinations.PermissionsScreenDestination
 import com.ramcosta.composedestinations.generated.destinations.SetupScreenDestination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import edu.rpi.shuttletracker.R
@@ -50,6 +53,16 @@ fun SettingsScreen(
     navigator: DestinationsNavigator,
     viewModel: SettingsViewModel = hiltViewModel(),
 ) {
+    val context = LocalContext.current
+
+    val openAppSettings: () -> Unit = {
+        val intent =
+            Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                data = Uri.fromParts("package", context.packageName, null)
+            }
+        context.startActivity(intent)
+    }
+
     val scrollBehavior =
         TopAppBarDefaults.exitUntilCollapsedScrollBehavior(
             rememberTopAppBarState(),
@@ -86,14 +99,6 @@ fun SettingsScreen(
 
             item {
                 SettingsItem(
-                    Icons.Outlined.PrivacyTip,
-                    stringResource(R.string.permissions),
-                    onClick = { navigator.navigate(PermissionsScreenDestination()) },
-                )
-            }
-
-            item {
-                SettingsItem(
                     Icons.Outlined.RestartAlt,
                     stringResource(R.string.redo_setup),
                     onClick = { navigator.navigate(SetupScreenDestination(true)) },
@@ -105,6 +110,14 @@ fun SettingsScreen(
                     Icons.Outlined.Info,
                     stringResource(R.string.about),
                     onClick = { navigator.navigate(AboutScreenDestination()) },
+                )
+            }
+
+            item {
+                SettingsItem(
+                    icon = Icons.Outlined.Settings,
+                    title = stringResource(R.string.open_app_settings),
+                    onClick = openAppSettings,
                 )
             }
 
