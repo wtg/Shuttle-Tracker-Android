e0ae77d932913561628e649641d4c66822044b0e
bryantran24 <158430748+bryantran24@users.noreply.github.com>
2026-04-14 16:00:41 -0400

    Bump version to 2.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "edu.rpi.shuttletracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "2.3.0"
+        versionCode = 15
+        versionName = "2.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
